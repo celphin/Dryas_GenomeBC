7cfffd8eaf573abdfa4c59012d760487659269e1
msandler7 <mis7@student.ubc.ca>
2023-05-25 17:25:00 -0700

    Updated Seedling_Metilene_notes

--- Seedlings_Phenology/Seedling_Metilene_Notes.bash
+++ Seedlings_Phenology/Seedling_Metilene_Notes.bash
@@ -13,14 +13,16 @@
 cp ~/projects/def-rieseber/Dryas_shared_data/CE_Seedling_metilene_input_bedGraphs/ ~/scratch/Seedling_Metilene
 
 
-########################################
+############################################################
 #Warming vs control DMRS
 
-#Metilene input prep:
 cd ~/scratch/Seedling_Metilene
+#In cedar5
 tmux new-session -s Seedling_Warming_DMRS
 tmux attach-session -t Seedling_Warming_DMRS
 
+#----------------------------------------
+#Metilene input prep:
 module load StdEnv/2020
 module load bedtools/2.30.0
 
@@ -36,13 +38,44 @@ module load bedtools/2.30.0
     #for bg in SE_*_${h2}*; do mv "$bg" "${h2}_${bg}"; done
 
 sh metilene_prep.sh 
+cd ..
 
+#----------------------------------------
 #Run metilene:
+salloc -c32 --time 2:50:00 --mem 120000m --account rpp-rieseber
 
-#
 
-######################################
-#High vs low DMRS
+#Warming control specific adjustments,
+#h1,h2: 
+    #h1="W", h2="C"
+#in_metilene="SE_metilene_"$h1"_"$h2".input"
+#input_dir=SE_${h1}_${h2}_input_files 
+#outputname=SE_"$h1"_"$h2"_"${maxdist}"_"${mincpgs}"_"${mindiff}"
+#parameters: maxdist, mincpgs, mindiff
+
+#metilene:
+# params: maxdist, mincpgs, mindiff
+module load StdEnv/2020
+module load bedtools/2.30.0 
+sh metilene_run.sh 70 5 4
+sh metilene_run.sh 150 5 4 
+sh metilene_run.sh 70 5 0.7
+
+#filter based on qval
+#params: maxdist, mincpgs, mindiff, minmeandif, q-value
+module load nixpkgs/16.09 
+module load gcc/7.3.0
+module load r/3.6.0
+module load gdal
+module load udunits
+module load python
+export R_LIBS_USER=/home/msandler/R/x86_64-pc-linux-gnu-library/3.6/
+sh metilene_filter_qval.sh 70 5 4 0.9 0.001
+sh metilene_filter_qval.sh 150 5 4 0.9 1e-5
+sh metilene_filter_qval.sh 70 5 0.7 0.7 0.001
+
+##############################################################
+#High vs low DMRS (incomplete, still left to do)
 #Metilene input prep:
 cd ~/scratch/Seedling_Metilene
 tmux new-session -s Seedling_Warming_DMRS
