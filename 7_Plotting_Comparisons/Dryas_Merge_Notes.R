8e9014a73c2889b8125f3bc2c11c2a4f3691d4d3
msandler7 <mis7@student.ubc.ca>
2023-11-17 15:48:53 -0800

    Updated Merge and Phenogram notes

--- 7_Plotting_Comparisons/Dryas_Merge_Notes.R
+++ 7_Plotting_Comparisons/Dryas_Merge_Notes.R
@@ -2,14 +2,16 @@
 #module load r/4.2.1
 #export R_LIBS_USER=/home/msandler/R/x86_64-pc-linux-gnu-library/4.2.1/
 #Need some memory for Wild_Lat data:
-#salloc -c1 --time 4:00:00 --mem 120000m --account def-rieseber
+#salloc -c1 --time 7:00:00 --mem 120000m --account def-rieseber
 #In R
 #To do:    
-    #Add instructions on how to preprocess files 
+    #Add instructions on how to preprocess files \
+    #RNA merge 
 #Can be run as script or just inputted into R
 
 library(dplyr)
 library(tidyr)
+library(stringr)
 #Get Alaska file:
 
 #####################################################################
@@ -29,7 +31,7 @@ process_gff3 <- function(file_name) {
     colnames(dryas_genes) <- gene_col_names
     #remove ID prefix:
     dryas_genes$Gene <- sub("^ID=", "", dryas_genes$Gene)
-    write.table(dryas_genes, "Dryas_Genes.csv", sep = "\t", quote = FALSE, row.names = FALSE)
+    #write.table(dryas_genes, "Dryas_Genes.csv", sep = "\t", quote = FALSE, row.names = FALSE)
     return(dryas_genes)
 
 
@@ -102,8 +104,6 @@ SE_W_C <- merge_metilene(Seedling_W_C_blast, "original_data/SE_W_C_70_5_4_0.9_qv
 SE_L_H <- merge_metilene(Seedling_L_H_blast, "original_data/SE_L_H_70_5_4_0.9_qval.0.001.out")
 print("Metilene loaded")
 ############################################################################
-#Works until here
-###########################################################################
 
 #Interproscan file processing
 process_interproscan_file <- function(interpro_file){
@@ -155,14 +155,16 @@ merge_go_names <- function(interproscan_terms, go_terms){
 
 collapsed_go_terms <- merge_go_names(interproscan_table, go_terms)
 
-##########################################################################
-#Modify DMR table
-gene_dmr_go_table$Origin <- sub("^original_data/", "", gene_dmr_go_table$Origin)
+
 ##########################################################################
 gene_dmr_table <- merge(dmr_table, dryas_genes, by = "Gene")
+#Modify DMR table
+gene_dmr_table$Origin <- sub("^original_data/", "", gene_dmr_table$Origin)
 write.table(gene_dmr_table, "Gene_DMR_Total_Merged_table.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
 ##########################################################################
 #Save dmrs with go table
+##########################################################################
+
 
 gene_dmr_go_table <- merge(gene_dmr_table, collapsed_go_terms, by = "Gene", all.x = TRUE)
 gene_dmr_go_table <- distinct(gene_dmr_go_table)
@@ -185,11 +187,48 @@ read_rna <- function(rna_file) {
 rna_table <- read_rna("original_data/RNA_DERS_Oct2023_W_C_Total_No_Headers.txt")
 #######################################################################
 #Attach GO_Terms, and names
+#!!! Start end did not work!!!
 rna_table <- merge(rna_table, dryas_genes, by = "Gene")
 rna_go_table <- merge(rna_table, collapsed_go_terms, by = "Gene", all.x = TRUE)
-print(head(rna_go_table))
-write.table(rna_go_table, "Gene_RNA_Total_GO_Merged_table.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+print(head(rna_go_table, n=20))
+
 
 #col names : Gene     logFC     logCPM        LR       PValue          fdr  Gene_Start Gene_End GO_Terms GO_Name
 
 #######################################################################
+#Edit RNA :
+rna_go_table <- rna_go_table %>% mutate(Origin = "RNA")
+rna_go_table <- rna_go_table %>% mutate(Scaffold = str_extract(Gene, "Do1_[^G]+"))
+print(head(rna_go_table))
+write.table(rna_go_table, "Gene_RNA_Total_GO_Merged_table.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+######################################################################
+#Combine:
+total_table <- bind_rows(gene_dmr_go_table, rna_go_table)
+print(head(total_table))
+
+write.table(total_table, "Gene_DMR_RNA_GO_Merged_table.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+
+#Get scaffold:
+###################################################################
+#Get Phenogram with origin as phenotype
+#total_table <- read.delim("Gene_DMR_RNA_GO_Merged_table.tsv", na.)
+pheno_table <- total_table[, c("Scaffold", "Gene_Start", "Origin")]
+print(head(pheno_table))
+pheno_table <- pheno_table[!duplicated(pheno_table), ]
+#result <- any(grepl("RNA", pheno_table$Origin))
+#print(result)
+
+#Extract true 
+rows_to_keep <- grepl("^Do1_[[:alnum:]]+_.*$", pheno_table$Scaffold)
+filtered_pheno_table <- pheno_table[rows_to_keep, ]
+print(head(filtered_pheno_table))
+colnames(filtered_pheno_table) <- c("CHR", "POS", "PHENOTYPE")
+result <- any(grepl("RNA", filtered_pheno_table$PHENOTYPE))
+print(result)
+#write.table(filtered_pheno_table, "Dryas_Total_Origin_Phenogram.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+filtered_pheno_table$CHR <- gsub(".*0(\\d+)_a0*(\\d+)", "\\1\\2", filtered_pheno_table$CHR)
+write.table(filtered_pheno_table, "Dryas_Total_Origin_Phenogram_Chr.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+###################################################################
+#Phenogram: RNA, True parent, seedling
+SE_W_C Parent_W_C
--- 7_Plotting_Comparisons/MS_Dryas_Phenogram.bash
+++ 7_Plotting_Comparisons/MS_Dryas_Phenogram.bash
@@ -1,9 +1,36 @@
 #########################################################
+#November 2023
 #Steps:
-    #Blast ref of all the original files (see blast ref file)
+    #Get 
     #Convert to Gene: start-end
     #Repeat with 
     #Get genome file for phenogram
 #########################################################
+
+
+wget https://ritchielab.org/files/RL_software/ruby_install.sh
+wget https://ritchielab.org/files/RL_software/pheno_gram.rb
+sh ruby_install.sh
+ruby pheno_gram.rb
+#######################################################
+#Acquire Dryas_Genome.txt:
+
+module load bioawk
+bioawk -c fastx '{ print $name, length($seq) }' < Dryas_octopetala_H1.supercontigs.fa
+
+#Hand Edit to be tsv file, with header:
+#######################################################
+grep -E "SE_W_C|Wild_W_C|Parent_W_C|POS|RNA" Dryas_Total_Origin_Phenogram_Chr.txt > Dryas_W_C_SE_Wild_P_RNA_Phenogram.txt   
+grep -E "SE_L_H|Wild_Lat_L_H|POS" Dryas_Total_Origin_Phenogram_Chr.txt > Dryas_L_H_SE_Wild_Phenogram.txt   
+grep -E "Sval|Alas|Swed|Nunavut|POS" Dryas_Total_Origin_Phenogram_Chr.txt > Dryas_Loc_W_C_Phenogram.txt   
+grep -E "Mat_Sen|POS" Dryas_Total_Origin_Phenogram_Chr.txt > Dryas_Phenology_Phenogram.txt  
+#######################################################
+ruby pheno_gram.rb -i Dryas_W_C_SE_Wild_P_RNA_Phenogram.txt -g Dryas_Genome.txt -t "Dryas Warming Phenogram" -o Dryas_W_C_SE_Wild_P_RNA_Phenogram -f jpg
+ruby pheno_gram.rb -i Dryas_Loc_W_C_Phenogram.txt -g Dryas_Genome.txt -t "Dryas Location Phenogram" -o Dryas_Loc_W_C_Phenogram -f jpg
+ruby pheno_gram.rb -i Dryas_Phenology_Phenogram.txt -g Dryas_Genome.txt -t "Dryas Phenology Phenogram" -o Dryas_Phenology_Phenogram -f jpg
+#Too many, figure out way to subset
+#ruby pheno_gram.rb -i Dryas_L_H_SE_Wild_Phenogram.txt -g Dryas_Genome.txt -t "Dryas Latitude Phenogram" -o Dryas_L_H_SE_Wild_Phenogram -f jpg
+#######################################################\
+scp -v msandler@cedar.computecanada.ca:/home/msandler/projects/def-rieseber/Dryas_shared_data/MS_Dryas_Phenogram/*.jpg .
 
 
